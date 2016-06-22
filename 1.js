3b835dc4a75d6b8fe5002a83a13ac2adf1e0b51b
mac039 <mac039@NQU-192-168-61-161.internal>
2016-06-22 08:59:06 +0800

    init

---
+++ 1.js
@@ -0,0 +1,11 @@
+				function m357(a,b){
+						for(var i=a;i<=b;i++){
+						if(i%3===0||i%5===0||i%7===0){
+							console.log(i);
+							
+						}
+						
+								
+					}
+				}
+				console.log(m357(1,15));
